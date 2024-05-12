023a63e7a81900cb3776b95eacd6983dd4524c3a
darinmilner <darin.milner@gmail.com>
2024-05-12 19:25:32 +0700

    Search Microservice Controllers added

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,10 +1,9 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
 using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -12,13 +11,11 @@ app.UseAuthentication();
 
 app.MapControllers();
 
-await DB.InitAsync("SearchDB", MongoClientSettings
-    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
-
-await DB.Index<Item>()
-    .Key(x => x.Make, KeyType.Text)
-    .Key(x => x.Model, KeyType.Text)
-    .Key(x => x.Color, KeyType.Text)
-    .CreateAsync();
+try{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e) {
+    Console.WriteLine(e);
+}
 
 app.Run();
